7f4115ea5d1bf3a58a40b40440cae7847e94d40f
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-15 09:14:34 +0600

    project boilerplate

---
+++ src/firebase.init.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqgkaNBYfZBQXvpFRBidcsFdg4kEKtbwg",
+  authDomain: "sign-in-project-6e3a5.firebaseapp.com",
+  projectId: "sign-in-project-6e3a5",
+  storageBucket: "sign-in-project-6e3a5.appspot.com",
+  messagingSenderId: "751142954729",
+  appId: "1:751142954729:web:0695f59343967953f2b278"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
